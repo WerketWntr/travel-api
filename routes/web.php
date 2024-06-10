37b823fa7e8a9077da1a622f7a9954de342bae2c
WerketWntr <blacky.m.raven@gmail.com>
2024-06-11 02:08:52 +0300

    login

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return ['Laravel' => app()->version()];
+});
+
+Route::prefix('/api/v1/admin')->middleware(['auth:sanctum', 'role:admin'])->group(function (){
+    Route::post('/travels', [\App\Http\Controllers\Api\V1\Admin\TravelController::class, 'store']);
+    Route::post('/travels/{travel}/tours', [\App\Http\Controllers\Api\V1\Admin\TourController::class, 'store']);
 });
 
 
+Route::get('/api/v1/travels', [\App\Http\Controllers\Api\V1\TravelController::class, 'index']);
+Route::get('/api/v1/travels/{travel:slug}/tours', [\App\Http\Controllers\Api\V1\TourController::class, 'index']);
+
+
+require __DIR__.'/auth.php';
